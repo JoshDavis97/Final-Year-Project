a8b1e2da4bd65d90c031b2c03bb3d0e16d31c70f
Josh Davis <josh.davis@hotmail.co.uk>
2019-02-11 14:38:39 +0000

    Removed redundant JS files, renamed main .css file.

--- js/Shop.js
+++
@@ -1,9 +0,0 @@
-class Shop {
-    constructor(name, address, rating, placeId, distance) {
-        this.name = name;
-        this.address = address;
-        this.rating = rating;
-        this.placeId = placeId;
-        this.distance = distance;
-    }
-}
